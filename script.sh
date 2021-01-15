4fba85979ffef27207bff5b1dceb81e502402688
Jason Freeberg <jafreebe@microsoft.com>
2021-01-15 10:53:26 -0800

    fix script vars

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
+#!/bin/sh
 
-$groupname="freebergARMDemo"
-$appname="freeberg-rfp-demo"
-
+groupname="freebergARMDemo"
+appname="freeberg-rfp-demo"
 
 az group create --name $groupname --location "southcentralus" &&
 az deployment group create --resource-group $groupname --parameters webAppName=$appname linuxFxVersion="NODE|10.15" \
