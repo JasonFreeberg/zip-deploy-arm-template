252ddf6af49f52b5bc1013be61e907c44b214ede
Jason Freeberg <jafreebe@microsoft.com>
2021-01-22 16:56:36 -0800

    attach logs

--- script.sh
+++ script.sh
@@ -5,4 +5,4 @@ appname="freeberg-rfp-demo"
 
 az group create --name $groupname --location "southcentralus" &&
 az deployment group create --resource-group $groupname --parameters webAppName=$appname linuxFxVersion="NODE|10.15" \
---template-file "template.json"
+--template-file "template.json" --debug
